d8994be73129aca7ed75f7f3f7b31701654bd630
SoNSoN2007 <hassan.0523096@gmail.com>
2025-01-27 22:50:27 +0200

    Create exercise4.sql

---
+++ exercise4.sql
@@ -0,0 +1,9 @@
+1)  INSERT INTO actor (first_name, last_name)
+VALUES ('اسمك', 'الأخير');
+
+2)   UPDATE film
+SET replacement_cost = replacement_cost + 10
+WHERE rating IN ('PG', 'G');
+
+3)   INSERT INTO film (title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating)
+VALUES ('اسم الفيلم', 'وصف الفيلم', 2025, 1, 5, 2.99, 120, 20.00, 'PG');
